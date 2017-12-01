2dcd0ff0b1d192495b3b112fd516dd05818d97be
kdj0c <kdj0c@djinvi.net>
2017-12-01 10:55:35 +0100

    Remove Gravity and Vibration special rules
    now AP can be in the form 2D6+2

--- onepagepoints.py
+++ onepagepoints.py
@@ -176,12 +176,6 @@ class Weapon:
                 ap += 0.5
             elif s == 'Indirect':
                 wrange *= 1.4
-            elif s == 'Vibration':
-                # Vibration weapons have AP(D6), so same cost as AP(4) looks fair
-                ap = 4
-            elif s == 'Gravity':
-                # Gravity weapons have AP(D3 + 1), so same cost as AP(3) looks fair
-                ap = 3
             elif s == 'Anti-Air':
                 sfactor *= 1.10
 
